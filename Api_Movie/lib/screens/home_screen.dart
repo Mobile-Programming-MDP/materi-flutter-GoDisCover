9e64d4ce24f6fc7bb2f2e0f979b11cf9fa348297
GoDisCover <95121614+GoDisCover@users.noreply.github.com>
2026-03-11 22:27:26 +0700

    Add files via upload

---
+++ Api_Movie/lib/screens/home_screen.dart
@@ -0,0 +1,82 @@
+import 'package:film/models/movie.dart';
+import 'package:film/service/api_service.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+
+  @override
+  final ApiService _apiService=ApiService();
+  Widget build(BuildContext context) {
+    List<Movie> _allMovies = [];
+    List<Movie> _trendingMovies = [];
+    List<Movie> _popularMovies = [];
+
+    Future<void> _loadMovies() async{
+      final List<Map<String,dynamic>> allMoviesData = await _apiService.getAllMovies();
+      final List<Map<String,dynamic>> trendingMoviesData = await _apiService.getTrendingMovies();
+      final List<Map<String,dynamic>> popularMoviesData = await _apiService.getPopularMovies();
+
+      setState(() {
+        _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+        _trendingMovies = trendingMoviesData.map((e) => Movie.fromJson(e)).toList();
+        _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+      });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Film"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _buildMovieList("All Movie", _allMovies),
+            _buildMovieList("Trending Movie", _trendingMovies),
+            _buildMovieList("Popular Movie", _popularMovies)
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _buildMovieList(String title, List<Movie> movies){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(padding: EdgeInsets.all(0.0),
+        child: Text(
+          title,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+        ),
+        ),
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (BuildContext context, int index){
+              final Movie movie = movies[index];
+              return Padding(padding: EdgeInsets.all(0.0),
+              child: Column(children: [
+                Image.network(
+                  movie.posterPath,
+                ),
+                SizedBox(height: 5,),
+                Text(movie.title, style: TextStyle(
+                  fontWeight: FontWeight.bold
+                ),)
+              ],),);
+            }
+          ),
+        )
+      ],
+    );
+  }
+}
